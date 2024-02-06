20c04f0060992d1b9acbaaa85e2b45236e91d883
Kevin Vu <kevinvu59@gmail.com>
2024-02-06 15:44:25 -0500

    Add endpoint path protocol for creating URL request and base defaults

---
+++ Toybox/Network/EndpointPath.swift
@@ -0,0 +1,34 @@
+//
+//  EndpointPath.swift
+//  SwiftUIToybox
+//
+//  Created by Kevin Vu on 2/6/24.
+//
+
+import Foundation
+
+protocol EndpointPath {
+    var method: String { get }
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var parameters: [URLQueryItem] { get }
+    var headers: [String: String] { get }
+    /// HTTP body, represented as `[String: Any]` which should be serialized to data before passing
+    /// to the `URLRequest`
+    var body: [String: Any] { get }
+}
+
+extension EndpointPath {
+    var scheme: String {
+        return "https"
+    }
+    
+    var host: String {
+        return "jsonplaceholder.typicode.com"
+    }
+    
+    var body: [String: Any] {
+        return [:]
+    }
+}
