a32215a62c31b34a8bf97c0980357923f56fb3e4
Kevin Vu <kevinvu59@gmail.com>
2024-02-06 14:29:03 -0500

    Add xcode files

---
+++ Toybox/SwiftUIToyboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToyboxApp.swift
+//  Toybox
+//
+//  Created by Kevin Vu on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToyboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
