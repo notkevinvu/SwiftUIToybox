a4dfe66b2e955a0dc4548410ddd88d68e2eabeaa
Kevin Vu <kevinvu59@gmail.com>
2024-02-08 01:01:25 -0500

    More fixes to dependency graph, folder re-organization for Data module

--- Data/Sources/Data/Networking/NetworkService.swift
+++ Data/Sources/Data/Networking/NetworkService.swift
@@ -6,24 +6,26 @@
 //
 
 import Foundation
+import Data
 
 public protocol NetworkService {
     var session: URLSession { get }
-    public func request<T: Decodable>(type: T.Type, endpoint: EndpointPath) async throws -> T
+    func request<T: Decodable>(type: T.Type, endpoint: EndpointPath) async throws -> T
 }
 
+@available(iOS 15.0, *)
 public class RestNetworkServiceAdapter: NetworkService {
-    var session: URLSession
+    public var session: URLSession
     
     public init(configuration: URLSessionConfiguration) {
         self.session = URLSession(configuration: configuration)
     }
     
-    convenience init() {
+    public convenience init() {
         self.init(configuration: .default)
     }
     
-    func request<T: Decodable>(type: T.Type, endpoint: EndpointPath) async throws -> T {
+    public func request<T: Decodable>(type: T.Type, endpoint: EndpointPath) async throws -> T {
         var components = URLComponents()
         components.scheme = endpoint.scheme
         components.host = endpoint.host
