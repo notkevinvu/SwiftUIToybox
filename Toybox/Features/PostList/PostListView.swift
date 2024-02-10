fbe58520184c61e2535ab494ff57bc91d298331f
Kevin Vu <kevinvu59@gmail.com>
2024-02-10 14:58:30 -0500

    Add view state enum to manage state logic. Also update view to follow

--- Toybox/Features/PostList/PostListView.swift
+++ Toybox/Features/PostList/PostListView.swift
@@ -13,14 +13,55 @@ struct PostListView: View {
     
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(viewModel.posts) { post in
-                    Text(post.title)
+            containerView
+                .navigationTitle("Posts")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Refresh") {
+                            Task {
+                                await viewModel.fetchPosts()
+                            }
+                        }
+                    }
                 }
-            }
         }
         .task {
             await viewModel.fetchPosts()
+        }
+    }
+    
+    @ViewBuilder
+    var containerView: some View {
+        switch viewModel.state {
+            case .initial:
+                // in theory, we would have some base view with
+                // skeleton views here that shimmer to replicate some
+                // data that hasn't loaded yet
+                EmptyView()
+                
+            case .loading:
+                ProgressView {
+                    Text("Loading posts")
+                }
+                
+            case .empty:
+                Text("No posts available!")
+                
+            case .success(let posts):
+                postListView(posts)
+                
+            case .error(let error):
+                Text("Error loading posts - \(error)")
+                
+        }
+    }
+    
+    @ViewBuilder
+    func postListView(_ posts: [Post]) -> some View {
+        List {
+            ForEach(posts) { post in
+                Text(post.title)
+            }
         }
     }
 }
--- Toybox/Features/PostList/PostListViewModel.swift
+++ Toybox/Features/PostList/PostListViewModel.swift
@@ -9,21 +9,35 @@ import Foundation
 
 final class PostListViewModel: ObservableObject {
     
+    // not sure if we want to keep this as a property here for the view
+    // to use or if we want to only allow the view to access these items
+    // through the success view state
+//    @Published private(set) var posts: [Post] = []
+    @Published private(set) var state: ViewState = .initial
     let postProvider: PostProvider
     
     init(postProvider: PostProvider) {
         self.postProvider = postProvider
     }
     
-    @Published var posts: [Post] = []
+    enum ViewState {
+        case initial
+        case loading
+        case empty
+        case success(posts: [Post])
+        case error(_ error: String)
+    }
     
     @MainActor
     func fetchPosts() async {
+        state = .loading
         do {
+            // delay task to show off loading animation
+            try await Task.sleep(for: .seconds(1))
             let posts = try await postProvider.fetchPosts()
-            self.posts = posts
+            state = .success(posts: posts)
         } catch {
-            print(error)
+            state = .error(error.localizedDescription)
         }
     }
 }
