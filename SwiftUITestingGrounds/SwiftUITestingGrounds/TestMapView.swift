012035fb36a6fdf1daf6cd5c30a425f8268d5582
Kevin Vu <kevinvu59@gmail.com>
2024-09-06 14:30:23 -0400

    Add test sheet for a sample search bar

--- SwiftUITestingGrounds/SwiftUITestingGrounds/TestMapView.swift
+++ SwiftUITestingGrounds/SwiftUITestingGrounds/TestMapView.swift
@@ -16,6 +16,9 @@ struct TestMapView: View {
     var body: some View {
 //        testMapView
         basicMapView
+            .sheet(isPresented: .constant(true)) {
+                MapSearchCompletionSheetView()
+            }
     }
     
     @ViewBuilder
@@ -109,7 +112,7 @@ struct TestMapView: View {
     TestMapView()
 }
 
-extension CLLocationCoordinate2D: Hashable {
+extension CLLocationCoordinate2D: @retroactive Hashable, @retroactive Equatable {
     public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
         lhs.latitude == rhs.latitude &&
         lhs.longitude == rhs.longitude
@@ -118,5 +121,68 @@ extension CLLocationCoordinate2D: Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(latitude)
         hasher.combine(longitude)
+    }
+}
+
+// MARK: -- Map search completion sheet
+struct MapSearchCompletionSheetView: View {
+    @State var mapSearchText = ""
+    @FocusState var searchFieldFocused: Bool
+    @State var shouldShowCancelButton: Bool = false
+    
+    private let smallDetent: PresentationDetent = .height(75)
+    @State private var currentSheetDetent: PresentationDetent = .height(75)
+    
+    private var availableDetents: Set<PresentationDetent> {
+        return [smallDetent, .medium]
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    TextField("Search for a location", text: $mapSearchText)
+                        .focused($searchFieldFocused, equals: true)
+                }
+                .padding(12)
+                .background(.gray.opacity(0.1))
+                .cornerRadius(8)
+                .foregroundStyle(.primary)
+                
+                if shouldShowCancelButton {
+                    Button("Cancel") {
+                        searchFieldFocused = false
+                        mapSearchText = ""
+                        // TODO: reset search results as well
+                    }
+                    .transition(.offset(x: 100))
+                }
+            }
+            
+            Spacer()
+        }
+        .animation(.spring(duration: 0.25), value: shouldShowCancelButton)
+        .onChange(of: searchFieldFocused) { oldValue, newValue in
+            shouldShowCancelButton = newValue
+            
+            if newValue == true {
+                currentSheetDetent = .medium
+            }
+        }
+        .onChange(of: currentSheetDetent) { oldValue, newValue in
+            if newValue == smallDetent {
+                searchFieldFocused = false
+            }
+        }
+        .padding(.horizontal)
+        .padding(.top, 16)
+        .interactiveDismissDisabled()
+        .presentationBackground(.regularMaterial)
+        .presentationBackgroundInteraction(.enabled)
+        .presentationDetents(
+            availableDetents,
+            selection: $currentSheetDetent
+        )
     }
 }
